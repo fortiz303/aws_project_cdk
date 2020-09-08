51a3432ec26ea464b4355eb3bf0ddc56ef5d7b58
francisco <francisco_ortiz1@yahoo.com>
2020-09-08 17:36:08 -0600

    added API gateway endpoint

--- cdkworkshop/cdkworkshop/cdkworkshop_stack.py
+++ cdkworkshop/cdkworkshop/cdkworkshop_stack.py
@@ -1,8 +1,7 @@
 from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
     aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
     core
 )
 
@@ -11,6 +10,16 @@ class CdkWorkshopStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='hello.handler',
+        )
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=my_lambda,
+        )
 #        queue = sqs.Queue(
 #            self, "CdkWorkshopQueue",
 #            visibility_timeout=core.Duration.seconds(300),
